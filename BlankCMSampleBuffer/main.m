9b7f2347b9801ba2882d10b9eb8ad0c80082dbc2
armen <absoboll@gmail.com>
2015-12-24 15:44:38 -0800

    Initial Commit

---
+++ BlankCMSampleBuffer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BlankCMSampleBuffer
+//
+//  Created by armen karamian on 12/24/15.
+//  Copyright © 2015 armen karamian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
